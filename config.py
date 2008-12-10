9543227d17caf05f780831b498ced5626ec7789f
Mario Amelung <mario.amelung@gmx.de>
2008-12-10 16:45:08 +0000

    Added an updated header (incl. author and copyright information); added logging.

--- config.py
+++ config.py
@@ -1,17 +1,28 @@
 # -*- coding: utf-8 -*-
+# $Id$
 #
-# File: ECAssignmentBox.py
+# Copyright (c) 2006-2008 Otto-von-Guericke-Universität Magdeburg
 #
-# Copyright (c) 2008 by []
-# Generator: ArchGenXML Version 2.1
-#            http://plone.org/products/archgenxml
+# This file is part of ECAssignmentBox.
 #
-# GNU General Public License (GPL)
+# ECAssignmentBox is free software; you can redistribute it and/or 
+# modify it under the terms of the GNU General Public License as 
+# published by the Free Software Foundation; either version 2 of the 
+# License, or (at your option) any later version.
 #
-
-__author__ = """unknown <unknown>"""
+# ECAssignmentBox is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ECAssignmentBox; if not, write to the 
+# Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, 
+# MA  02110-1301  USA
+#
+__author__ = """Mario Amelung <mario.amelung@gmx.de>"""
 __docformat__ = 'plaintext'
-
+__version__   = '$Revision$'
 
 # Product configuration.
 #
@@ -25,7 +36,6 @@ __docformat__ = 'plaintext'
 from Products.CMFCore.permissions import setDefaultRoles
 ##code-section config-head #fill in your manual code here
 ##/code-section config-head
-
 
 PROJECTNAME = "ECAssignmentBox"
 
